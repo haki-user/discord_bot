a1b57b554460a707aca2ff492e7b42751cdb1a5a
haki-user <adityapratapsingh33@outlook.com>
2023-06-07 23:41:27 +0530

    modified:   commands/aboutMe.js

--- commands/aboutMe.js
+++ commands/aboutMe.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const message = new EmbedBuilder()
     .setColor(0x0099FF)
     .setTitle('Aditya')
-    .setURL('https://github.com/haki-user');
+    .setURL('https://github.com/haki-user/discord_bot_bankai');
 
 module.exports = {
     data: new SlashCommandBuilder()
